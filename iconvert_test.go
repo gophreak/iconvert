e0ad1d5ddb4e42d287db87a9af4e0fd956373fc8
h4xorpl <igor.filipek@purplewifi.com>
2018-04-13 18:08:35 +0100

    refactored test to be test-tables

--- iconvert_test.go
+++ iconvert_test.go
@@ -1,245 +1,295 @@
 package iconvert_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/gophreak/iconvert"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestToStringWithString(t *testing.T) {
-	value := "1"
-
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "1", v)
+func messageForFailedTest(index int, message string) string {
+	return fmt.Sprintf("test case: [%v] with description: %s", index, message)
 }
 
-func TestToStringWithInt(t *testing.T) {
-	value := int(6)
+func TestToString(t *testing.T) {
+	cases := []struct {
+		input                interface{}
+		result               string
+		wantError            bool
+		errorMessageContains string
+		description          string
+	}{
+		{
+			"Hello",
+			"Hello",
+			false,
+			"",
+			"convert string that is not a number",
+		},
+		{
+			struct{ A int }{A: 5},
+			"",
+			true,
+			"Value struct { A int } is type struct",
+			"convert struct to int",
+		},
+		{
+			float64(359.8999999999999772626324556767940521240234375),
+			"359.9",
+			false,
+			"",
+			"convert float64",
+		},
+		{
+			float32(359.899993896484375),
+			"359.9",
+			false,
+			"",
+			"convert float32",
+		},
+		{
+			int64(9223372036854775807),
+			"9223372036854775807",
+			false,
+			"",
+			"convert int64",
+		},
+		{
+			int32(2147483647),
+			"2147483647",
+			false,
+			"",
+			"convert int32",
+		},
+		{
+			int16(32767),
+			"32767",
+			false,
+			"",
+			"convert int16",
+		},
+		{
+			int8(127),
+			"127",
+			false,
+			"",
+			"convert int8",
+		},
+		{
+			int(6),
+			"6",
+			false,
+			"",
+			"convert int",
+		},
+		{
+			"1",
+			"1",
+			false,
+			"",
+			"convert string",
+		},
+	}
+	for i, tt := range cases {
+		v, e := iconvert.ToString(tt.input)
+		if tt.wantError {
+			assert.Errorf(t, e, messageForFailedTest(i, tt.description))
+			if tt.errorMessageContains != "" && e != nil {
+				assert.Containsf(t, e.Error(), tt.errorMessageContains, messageForFailedTest(i, tt.description))
+			}
 
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "6", v)
+			continue
+		}
+		assert.NoErrorf(t, e, messageForFailedTest(i, tt.description))
+		assert.Equalf(t, tt.result, v, messageForFailedTest(i, tt.description))
+	}
 }
 
-func TestToStringWithInt8(t *testing.T) {
-	value := int8(127)
+func TestToFloat(t *testing.T) {
+	cases := []struct {
+		input                interface{}
+		result               float64
+		wantError            bool
+		errorMessageContains string
+		description          string
+	}{
+		{
+			"Hello",
+			0.0,
+			true,
+			"invalid syntax",
+			"convert string that is not a number",
+		},
+		{
+			struct{ A int }{A: 5},
+			0.0,
+			true,
+			"Value struct { A int } is type struct",
+			"convert struct to int",
+		},
+		{
+			float64(179769313.4862932),
+			179769313.4862932,
+			false,
+			"",
+			"convert float64",
+		},
+		{
+			float32(359.9),
+			359.9,
+			false,
+			"",
+			"convert float32",
+		},
+		{
+			int64(9223372036854775807),
+			9223372036854775807.0,
+			false,
+			"",
+			"convert int64",
+		},
+		{
+			int32(2147483647),
+			2147483647.0,
+			false,
+			"",
+			"convert int32",
+		},
+		{
+			int16(32767),
+			32767.0,
+			false,
+			"",
+			"convert int16",
+		},
+		{
+			int8(127),
+			127.0,
+			false,
+			"",
+			"convert int8",
+		},
+		{
+			int(6),
+			6.0,
+			false,
+			"",
+			"convert int",
+		},
+		{
+			"1",
+			1.0,
+			false,
+			"",
+			"convert string",
+		},
+	}
+	for i, tt := range cases {
+		v, e := iconvert.ToFloat(tt.input)
+		if tt.wantError {
+			assert.Errorf(t, e, messageForFailedTest(i, tt.description))
+			if tt.errorMessageContains != "" {
+				assert.Containsf(t, e.Error(), tt.errorMessageContains, messageForFailedTest(i, tt.description))
+			}
 
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "127", v)
+			continue
+		}
+		assert.NoErrorf(t, e, messageForFailedTest(i, tt.description))
+		assert.Equalf(t, float64(tt.result), v, messageForFailedTest(i, tt.description))
+	}
 }
 
-func TestToStringWithInt16(t *testing.T) {
-	value := int16(32767)
+func TestToInt(t *testing.T) {
+	cases := []struct {
+		input                interface{}
+		result               int64
+		wantError            bool
+		errorMessageContains string
+		description          string
+	}{
+		{
+			"Hello",
+			0,
+			true,
+			"invalid syntax",
+			"convert string that is not a number",
+		},
+		{
+			struct{ A int }{A: 5},
+			0,
+			true,
+			"Value struct { A int } is type struct",
+			"convert struct to int",
+		},
+		{
+			float64(179769313.4862932),
+			179769313,
+			false,
+			"",
+			"convert float64",
+		},
+		{
+			float32(359.9),
+			359,
+			false,
+			"",
+			"convert float32",
+		},
+		{
+			int64(9223372036854775807),
+			9223372036854775807,
+			false,
+			"",
+			"convert int64",
+		},
+		{
+			int32(2147483647),
+			2147483647,
+			false,
+			"",
+			"convert int32",
+		},
+		{
+			int16(32767),
+			32767,
+			false,
+			"",
+			"convert int16",
+		},
+		{
+			int8(127),
+			127,
+			false,
+			"",
+			"convert int8",
+		},
+		{
+			int(6),
+			6,
+			false,
+			"",
+			"convert int",
+		},
+		{
+			"1",
+			1,
+			false,
+			"",
+			"convert string",
+		},
+	}
+	for i, tt := range cases {
+		v, e := iconvert.ToInt(tt.input)
+		if tt.wantError {
+			assert.Errorf(t, e, messageForFailedTest(i, tt.description))
+			if tt.errorMessageContains != "" {
+				assert.Containsf(t, e.Error(), tt.errorMessageContains, messageForFailedTest(i, tt.description))
+			}
 
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "32767", v)
-}
-
-func TestToStringWithInt32(t *testing.T) {
-	value := int32(2147483647)
-
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "2147483647", v)
-}
-
-func TestToStringWithInt64(t *testing.T) {
-	value := int64(9223372036854775807)
-
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "9223372036854775807", v)
-}
-
-func TestToStringWithFloat32(t *testing.T) {
-	value := float32(359.899993896484375)
-
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "359.9", v)
-}
-
-func TestToStringWithFloat64(t *testing.T) {
-	value := float64(359.8999999999999772626324556767940521240234375)
-
-	v, e := iconvert.ToString(value)
-	assert.NoError(t, e)
-	assert.Equal(t, "359.9", v)
-}
-
-func TestToStringWithStruct(t *testing.T) {
-	value := struct{ A int }{A: 5}
-
-	v, e := iconvert.ToString(value)
-	assert.Error(t, e)
-	assert.Equal(t, e.Error(), "Value struct { A int } is type struct")
-	assert.Equal(t, "", v)
-}
-
-func TestToFloatWithString(t *testing.T) {
-	value := "1"
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(1), v)
-}
-
-func TestToFloatWithInt(t *testing.T) {
-	value := int(6)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(6), v)
-}
-
-func TestToFloatWithInt8(t *testing.T) {
-	value := int8(127)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(127), v)
-}
-
-func TestToFloatWithInt16(t *testing.T) {
-	value := int16(32767)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(32767), v)
-}
-
-func TestToFloatWithInt32(t *testing.T) {
-	value := int32(2147483647)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(2147483647), v)
-}
-
-func TestToFloatWithInt64(t *testing.T) {
-	value := int64(9223372036854775807)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(9223372036854775807), v)
-}
-
-func TestToFloatWithFloat32(t *testing.T) {
-	value := float32(359.9)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(359.9), v)
-}
-
-func TestToFloatWithFloat64(t *testing.T) {
-	value := float64(179769313.4862932)
-
-	v, e := iconvert.ToFloat(value)
-	assert.NoError(t, e)
-	assert.Equal(t, float64(179769313.4862932), v)
-}
-
-func TestToFloatWithStruct(t *testing.T) {
-	value := struct{ A int }{A: 5}
-
-	v, e := iconvert.ToFloat(value)
-	assert.Error(t, e)
-	assert.Equal(t, e.Error(), "Value struct { A int } is type struct")
-	assert.Equal(t, float64(0), v)
-}
-
-func TestToFloatWithWord(t *testing.T) {
-	value := "Hello"
-
-	v, e := iconvert.ToFloat(value)
-	assert.Error(t, e)
-	assert.Contains(t, e.Error(), "invalid syntax")
-	assert.Equal(t, float64(0), v)
-}
-
-func TestToIntWithString(t *testing.T) {
-	value := "1"
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(1), v)
-}
-
-func TestToIntWithInt(t *testing.T) {
-	value := int(6)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(6), v)
-}
-
-func TestToIntWithInt8(t *testing.T) {
-	value := int8(127)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(127), v)
-}
-
-func TestToIntWithInt16(t *testing.T) {
-	value := int16(32767)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(32767), v)
-}
-
-func TestToIntWithInt32(t *testing.T) {
-	value := int32(2147483647)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(2147483647), v)
-}
-
-func TestToIntWithInt64(t *testing.T) {
-	value := int64(9223372036854775807)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(9223372036854775807), v)
-}
-
-func TestToIntWithFloat32(t *testing.T) {
-	value := float32(359.9)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(359), v)
-}
-
-func TestToIntWithFloat64(t *testing.T) {
-	value := float64(179769313.4862932)
-
-	v, e := iconvert.ToInt(value)
-	assert.NoError(t, e)
-	assert.Equal(t, int64(179769313), v)
-}
-
-func TestToIntWithStruct(t *testing.T) {
-	value := struct{ A int }{A: 5}
-
-	v, e := iconvert.ToInt(value)
-	assert.Error(t, e)
-	assert.Equal(t, e.Error(), "Value struct { A int } is type struct")
-	assert.Equal(t, int64(0), v)
-}
-
-func TestToIntWithWord(t *testing.T) {
-	value := "Hello"
-
-	v, e := iconvert.ToInt(value)
-	assert.Error(t, e)
-	assert.Contains(t, e.Error(), "invalid syntax")
-	assert.Equal(t, int64(0), v)
+			continue
+		}
+		assert.NoErrorf(t, e, messageForFailedTest(i, tt.description))
+		assert.Equalf(t, int64(tt.result), v, messageForFailedTest(i, tt.description))
+	}
 }
